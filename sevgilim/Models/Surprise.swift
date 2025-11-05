6d80c9c88421dc7678be67df71761584acfe8810
adilemreee <adil.emre@hotmail.com>
2025-11-06 02:15:00 +0300

    h

--- sevgilim/Models/Surprise.swift
+++ sevgilim/Models/Surprise.swift
@@ -35,6 +35,50 @@ struct Surprise: Identifiable, Codable {
         case isManuallyHidden
     }
     
+    init(
+        id: String? = nil,
+        relationshipId: String,
+        title: String,
+        message: String,
+        photoURL: String? = nil,
+        revealDate: Date,
+        createdBy: String,
+        createdFor: String,
+        createdAt: Date,
+        isOpened: Bool,
+        openedAt: Date? = nil,
+        isManuallyHidden: Bool = false
+    ) {
+        self.id = id
+        self.relationshipId = relationshipId
+        self.title = title
+        self.message = message
+        self.photoURL = photoURL
+        self.revealDate = revealDate
+        self.createdBy = createdBy
+        self.createdFor = createdFor
+        self.createdAt = createdAt
+        self.isOpened = isOpened
+        self.openedAt = openedAt
+        self.isManuallyHidden = isManuallyHidden
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        _id = try container.decode(DocumentID<String>.self, forKey: .id)
+        relationshipId = try container.decode(String.self, forKey: .relationshipId)
+        title = try container.decode(String.self, forKey: .title)
+        message = try container.decode(String.self, forKey: .message)
+        photoURL = try container.decodeIfPresent(String.self, forKey: .photoURL)
+        revealDate = try container.decode(Date.self, forKey: .revealDate)
+        createdBy = try container.decode(String.self, forKey: .createdBy)
+        createdFor = try container.decode(String.self, forKey: .createdFor)
+        createdAt = try container.decode(Date.self, forKey: .createdAt)
+        isOpened = try container.decodeIfPresent(Bool.self, forKey: .isOpened) ?? false
+        openedAt = try container.decodeIfPresent(Date.self, forKey: .openedAt)
+        isManuallyHidden = try container.decodeIfPresent(Bool.self, forKey: .isManuallyHidden) ?? false
+    }
+    
     // Sürprizin kilitli olup olmadığını kontrol et
     var isLocked: Bool {
         if isManuallyHidden {
